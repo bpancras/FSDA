7317c04fc8967ee4cc804f229d053ee3027dd585
Bensingh Pancras <bpancras@mathworks.com>
2024-05-09 15:16:02 +0530

    Minor update to addtOptions

--- toolbox/regression/addtOptions.m
+++ toolbox/regression/addtOptions.m
@@ -1,13 +1,13 @@
 classdef addtOptions
 
     properties
-        intercept (1,1) logical =true 
-        la (1,1) double =NaN
-        nocheck (1,1) logical =false
-        plots (1,1) double =0
-        FontSize (1,1) double =10
-        SizeAxesNum (1,1) double =10
-        textlab (1,1) logical =false
-        units (:,:) double =[]
+        intercept (1,1) logical = true 
+        la (1,1) double = NaN
+        nocheck (1,1) logical = false
+        plots (1,1) double = 0
+        FontSize (1,1) double = 10
+        SizeAxesNum (1,1) double = 10
+        textlab (1,1) logical = false
+        units (:,:) double = []
     end
 end
